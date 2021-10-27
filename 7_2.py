6eec34e52e36c15e1b9f5c8bdde06d5c2cf68031
Komissarzhevskaya <91774305+Komissarzhevskaya@users.noreply.github.com>
2021-10-27 03:03:59 +0300

    Create 7_2.py

---
+++ 7_2.py
@@ -0,0 +1,23 @@
+import os
+import yaml
+"""Сначала сделала через чтение отступов (решение мне не понравилось). Посмотрела разбор задания и разобрала для себя"""
+
+
+def tree_dirs(dir_dict, directory=''):
+    for key, val in dir_dict.items():
+        dir_path = os.path.join(directory, key)
+        os.makedirs(dir_path, exist_ok=True)
+        if isinstance(val, dict):
+            tree_dirs(val, dir_path)
+        else:
+            for i in val:
+                if isinstance(i, str):
+                    with open(os.path.join(dir_path, i), 'w') as f:
+                        pass
+                else:
+                    tree_dirs(i, dir_path)
+
+
+with open('config.yaml', 'r') as f:
+    yaml_dict = yaml.safe_load(f)
+    tree_dirs(yaml_dict)
